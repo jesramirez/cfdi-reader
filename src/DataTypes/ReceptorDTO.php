03136e56eec76d106c778c2cfd29c2e849385bc7
Fabian Juarez <fabian.main@gmail.com>
2017-02-01 00:48:42 -0600

    Heredar propiedades desde clase abstracta para contribuyente

--- src/DataTypes/ReceptorDTO.php
+++ src/DataTypes/ReceptorDTO.php
@@ -8,47 +8,7 @@ namespace CFDI\Reader\DataTypes;
  * Date: 29/01/2017
  * Time: 09:27 PM
  */
-class ReceptorDTO
+class ReceptorDTO extends ContribuyenteAbstractDTO
 {
-    /**
-     * @var
-     */
-    protected $nombre;
 
-    /**
-     * @var
-     */
-    protected $rfc;
-
-    /**
-     * @return mixed
-     */
-    public function getNombre()
-    {
-        return $this->nombre;
-    }
-
-    /**
-     * @param mixed $nombre
-     */
-    public function setNombre($nombre)
-    {
-        $this->nombre = $nombre;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getRfc()
-    {
-        return $this->rfc;
-    }
-
-    /**
-     * @param mixed $rfc
-     */
-    public function setRfc($rfc)
-    {
-        $this->rfc = $rfc;
-    }
 }
