adc0fb93a2670b11ecfe91ef889119d967dadc89
Fabian Juarez <fabian.main@gmail.com>
2017-02-01 00:45:45 -0600

    DTO para regimen fiscal

---
+++ src/DataTypes/RegimenFiscalDTO.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CFDI\Reader\DataTypes;
+
+/**
+ * User: @fabianjuarezmx
+ * Date: 01/02/2017
+ * Time: 12:43 AM
+ */
+class RegimenFiscalDTO
+{
+    /**
+     * Regimen Fiscal
+     * @var string
+     */
+    protected $regimen;
+
+    /**
+     * @return string
+     */
+    public function getRegimen()
+    {
+        return $this->regimen;
+    }
+
+    /**
+     * @param string $regimen
+     * @return RegimenFiscalDTO
+     */
+    public function setRegimen($regimen)
+    {
+        $this->regimen = $regimen;
+        return $this;
+    }
+}
