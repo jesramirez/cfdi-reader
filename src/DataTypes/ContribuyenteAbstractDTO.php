6b9cd46fac46ead0423589b9772da9d58e896e3e
Fabian Juarez <fabian.main@gmail.com>
2017-02-01 00:17:25 -0600

    Clase abstracta para atributos comunes al contribuyente

---
+++ src/DataTypes/ContribuyenteAbstractDTO.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace CFDI\Reader\DataTypes;
+
+/**
+ * User: @fabianjuarezmx
+ * Date: 31/01/2017
+ * Time: 11:55 PM
+ */
+abstract class ContribuyenteAbstractDTO
+{
+    /**
+     * Razón social
+     * @var string
+     */
+    protected $nombre;
+
+    /**
+     * Registro Federal de Contribuyentes
+     * @var string
+     */
+    protected $rfc;
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     * @return ContribuyenteAbstractDTO
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRfc()
+    {
+        return $this->rfc;
+    }
+
+    /**
+     * @param string $rfc
+     * @return ContribuyenteAbstractDTO
+     */
+    public function setRfc($rfc)
+    {
+        $this->rfc = $rfc;
+        return $this;
+    }
+}
