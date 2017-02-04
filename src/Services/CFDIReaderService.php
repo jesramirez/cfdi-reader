8834abd4c7bceba85cf39f8459b981d608400a9a
Fabian Juarez <fabian.main@gmail.com>
2017-02-03 22:23:27 -0600

    Definicion de metodos disponibles para servicio de lectura de CFDI

---
+++ src/Services/CFDIReaderService.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * User: @fabianjuarezmx
+ * Date: 29/01/2017
+ * Time: 04:31 PM
+ */
+class CFDIReaderService
+{
+    public function readFromFilePath($filePath)
+    {
+        // todo implement
+    }
+
+    public function readFromXMLString()
+    {
+        // todo implement
+    }
+}
